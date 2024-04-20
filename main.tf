d1c34ec24c14dc99c23939faec6c53db4465b66a
marom83 <marom83@gmail.com>
2024-04-20 11:26:15 +0200

    #

--- main.tf
+++ main.tf
@@ -1,1 +1,15 @@
+module "files" {
+  source = "./modules/files"
+}
+
+module "read" {
+  source = "./modules/read"
+  depends_on = [
+    module.files
+  ]
+}
+
+module "read" {
+  source = "./modules/read"
+}
 
